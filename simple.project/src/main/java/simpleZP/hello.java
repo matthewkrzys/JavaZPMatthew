6be4eff30595349a207fda44e0bbdcb20096c786
Matthew Krzys <matthewkrzys@gmail.com>
2018-03-07 21:33:15 +0100

    first commit

---
+++ simple.project/src/main/java/simpleZP/hello.java
@@ -0,0 +1,12 @@
+package simpleZP;
+
+import org.apache.commons.lang.math.RandomUtils;
+
+
+public class hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Random int >>> " + RandomUtils.nextInt());
+    }
+}
